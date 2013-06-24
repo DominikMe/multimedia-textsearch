ead0f8cd44c02b74f7be2dc3cd0f9b73d53eea90
Dominik Messinger <Dome@Dome-Laptop.(none)>
2013-06-24 19:40:45 +0200

    first inverted-index draft with xml (de)seriazilition ability

---
+++ Indexer/src/edu/kit/iti/algo2/textindexing/alexdomge/index/InvertedIndex.java
@@ -0,0 +1,192 @@
+package edu.kit.iti.algo2.textindexing.alexdomge.index;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class InvertedIndex {
+	private Map<String, List<Occurrence>> index;
+
+	public InvertedIndex() {
+		index = new HashMap<String, List<Occurrence>>();
+	}
+
+	public void put(String word, int docID) {
+		if (index.get(word) == null) {
+			index.put(word, new ArrayList<Occurrence>());
+		}
+		updateOrInsertOccurence(word, docID);
+	}
+
+	private void updateOrInsertOccurence(String word, int docID) {
+		List<Occurrence> list = index.get(word);
+		if (!list.isEmpty()) {
+			Occurrence occ = list.get(list.size() - 1);
+			if (occ.docID == docID) {
+				occ.count++;
+				return;
+			}
+		}
+		Occurrence occ = new Occurrence(docID);
+		index.get(word).add(occ);
+	}
+
+	public void saveXML(String filename) {
+		IndexXMLProcessor.writeFile(filename, index);
+	}
+
+	public void loadXML(String filename) {
+		index = IndexXMLProcessor.readFile(filename);
+	}
+
+	static class Occurrence {
+		final int docID;
+		int count = 1;
+
+		public Occurrence(int docID) {
+			this.docID = docID;
+		}
+	}
+
+	public static void main(String[] args) {
+		InvertedIndex inv = new InvertedIndex();
+		inv.loadXML("index.xml");
+		return;
+	}
+
+	static class IndexXMLProcessor {
+
+		private static final String COUNT = "count";
+		private static final String DOCUMENT = "document";
+		private static final String OCCURRENCE = "occurrence";
+		private static final String WORD = "word";
+		private static final String ENTRY = "entry";
+		private static final String INDEX = "index";
+
+		static Map<String, List<Occurrence>> readFile(String filename) {
+			Map<String, List<Occurrence>> index = new HashMap<String, List<Occurrence>>();
+			XMLStreamReader parser = null;
+			try {
+				parser = XMLInputFactory.newInstance().createXMLStreamReader(
+						new FileInputStream(filename));
+
+				List<Occurrence> list = null;
+				while (parser.hasNext()) {
+					if (parser.getEventType() != XMLStreamConstants.START_ELEMENT) {
+						parser.next();
+						continue;
+					}
+					String name = parser.getLocalName();
+					String word;
+					if (name.equals(ENTRY)) {
+						word = parser.getAttributeValue(null, WORD);
+						list = new ArrayList<Occurrence>();
+						index.put(word, list);
+					} else if (name.equals(OCCURRENCE)) {
+						int docID = Integer.parseInt(parser.getAttributeValue(
+								null, DOCUMENT));
+						int count = Integer.parseInt(parser.getAttributeValue(
+								null, COUNT));
+						Occurrence occ = new InvertedIndex.Occurrence(docID);
+						occ.count = count;
+						list.add(occ);
+					}
+					parser.next();
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (XMLStreamException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FactoryConfigurationError e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					parser.close();
+				} catch (XMLStreamException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return index;
+		}
+
+		static void writeFile(String filename,
+				Map<String, List<Occurrence>> index) {
+
+			try {
+				Document dom = DocumentBuilderFactory.newInstance()
+						.newDocumentBuilder().newDocument();
+				Element root = dom.createElement(INDEX);
+				for (Entry<String, List<Occurrence>> wordEntry : index
+						.entrySet()) {
+					Element wordNode = dom.createElement(ENTRY);
+					wordNode.setAttribute(WORD, wordEntry.getKey());
+					for (Occurrence occ : wordEntry.getValue()) {
+						Element occurenceNode = dom.createElement(OCCURRENCE);
+						occurenceNode.setAttribute(DOCUMENT, "" + occ.docID);
+						occurenceNode.setAttribute(COUNT, "" + occ.count);
+						wordNode.appendChild(occurenceNode);
+					}
+					root.appendChild(wordNode);
+				}
+				dom.appendChild(root);
+
+				Transformer transf = TransformerFactory.newInstance()
+						.newTransformer();
+				transf.setOutputProperty(OutputKeys.INDENT, "yes");
+				transf.setOutputProperty(OutputKeys.METHOD, "xml");
+				transf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+				transf.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+				transf.setOutputProperty(
+						"{http://xml.apache.org/xslt}indent-amount", "4");
+
+				transf.transform(new DOMSource(dom), new StreamResult(
+						new FileOutputStream(filename)));
+
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerFactoryConfigurationError e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
---
+++ Indexer/src/edu/kit/iti/algo2/textindexing/alexdomge/indexer/IndexerMain.java
@@ -0,0 +1,107 @@
+package edu.kit.iti.algo2.textindexing.alexdomge.indexer;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kit.iti.algo2.textindexing.alexdomge.index.InvertedIndex;
+
+public class IndexerMain {
+
+	private static final String FILE_ENDING = ".trscrpt";
+
+	public static void main(String[] args) {
+		IndexBuilder builder = new IndexBuilder();
+
+		List<String> fileNames = new ArrayList<String>();
+		String out = null;
+
+		try {
+			int i = 0;
+			while (i < args.length) {
+				switch (args[i]) {
+				case "-o":
+					out = args[++i];
+					break;
+				case "-i":
+					while (!args[++i].startsWith("-"))
+						fileNames.add(args[i]);
+					i--;
+					break;
+				default:
+					showUsageHint();
+					return;
+				}
+				i++;
+			}
+		} catch (IndexOutOfBoundsException e) {
+			showError();
+			showUsageHint();
+		}
+
+		if (fileNames.isEmpty()) {
+			System.out.println("No input defined!");
+			showUsageHint();
+			return;
+		}
+
+		List<File> files = validateInputFiles(fileNames);
+		if (out == null) {
+			out = "index_" + System.currentTimeMillis() + ".xml";
+		} else if (!out.endsWith(".xml"))
+			out += ".xml";
+
+		builder.addDocuments(files.toArray(new File[0]));
+		System.out.println("Build inverted index...");
+		InvertedIndex index = builder.build();
+		System.out.println("Done.");
+		System.out.println("Write output to " + out);
+		index.saveXML(out);
+	}
+
+	private static List<File> validateInputFiles(List<String> in) {
+		List<File> files = new ArrayList<File>();
+		for (String file : in) {
+			File f = new File(file);
+			if (!f.exists()) {
+				System.out.println("File " + file + " does not exist!");
+				return null;
+			}
+			if (file.endsWith(FILE_ENDING)) {
+				files.add(f);
+			} else {
+				if (!f.isDirectory()) {
+					System.out.println(file
+							+ " is neither a directory nor does it end on "
+							+ FILE_ENDING + "!");
+					return null;
+				} else {
+					File[] children = f.listFiles(new FilenameFilter() {
+
+						@Override
+						public boolean accept(File dir, String name) {
+							return name.endsWith(FILE_ENDING);
+						}
+					});
+					for (File c : children) {
+						files.add(c);
+					}
+				}
+			}
+		}
+		return files;
+	}
+
+	private static void showUsageHint() {
+		System.out
+				.println("Usage: -i File1 File2 Dir1 File3 ... -o output.xml");
+		System.out
+				.println("Important: Input must be " + FILE_ENDING + " files");
+	}
+
+	private static void showError() {
+		System.out.println("Malformed command!");
+	}
+
+}
